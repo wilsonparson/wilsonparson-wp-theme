1b2680623d275ddf3e5c18e3e4a236aeaa0a2122
Wilson Parson <wilson.parson@atomicfi.com>
2023-07-27 15:15:49 -0600

    chore: enqueue global styles

--- functions.php
+++ functions.php
@@ -1,3 +1,10 @@
 <?php
 
+function enqueue_global_styles()
+{
+    wp_enqueue_style('global', get_template_directory_uri() . '/css/style.css');
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_global_styles');
+
 add_theme_support('title-tag');
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
-
+<h1>Hi</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium quia libero distinctio dignissimos non, maiores laboriosam qui illo, nam laborum provident dolorem officiis sapiente accusamus dolor optio, quam eos!</p>
 <?php get_footer(); ?>
