287ab2eea7322484380b1a2ff75198e4e825dcd9
Wilson Parson <wilson.parson@atomicfi.com>
2025-03-05 22:08:48 -0700

    fix(perf): dequeue block library styles

--- functions.php
+++ functions.php
@@ -6,7 +6,19 @@ function enqueue_global_styles()
     wp_enqueue_style('global', get_template_directory_uri() . '/css/style.css', array(), $theme_version);
 }
 
-add_action('wp_enqueue_scripts', 'enqueue_global_styles');
+function dequeue_block_library_styles()
+{
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+}
+
+function on_wp_enqueue_scripts()
+{
+    dequeue_block_library_styles();
+    enqueue_global_styles();
+}
+
+add_action('wp_enqueue_scripts', 'on_wp_enqueue_scripts');
 
 add_theme_support('title-tag');
 
